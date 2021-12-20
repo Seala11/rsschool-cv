2ab3f5fa26607fc3c38276292c9887e99129f4ad
Hanna Papova <seal.annuska@gmail.com>
2021-12-20 16:52:33 -0300

    feat: add page layout

---
+++ script.js
@@ -0,0 +1,32 @@
+const navList = document.getElementById('primary-nav');
+const navElement = document.querySelector('.nav');
+const navToggleButton = document.querySelector('.mobile-nav-toggle');
+
+// menu open/close toggle
+const navHandler = () => {
+  const visibility = navList.getAttribute('data-visible');
+
+  if (visibility === 'false') {
+    navList.setAttribute('data-visible', true);
+    navToggleButton.setAttribute('aria-expended', true);
+    navToggleButton.innerHTML = `Close`;
+  } else {
+    navList.setAttribute('data-visible', false);
+    navToggleButton.setAttribute('aria-expended', false);
+    navToggleButton.innerHTML = `Menu`;
+  }
+};
+
+//close menu by pressing menu links
+const navLinkHandler = (element) => {
+  const linkElement = element.target.closest('.nav-list__item');
+
+  if (!linkElement) return;
+
+//   setTimeout(navHandler, 250);
+  navHandler()
+  
+};
+
+navElement.addEventListener('click', navLinkHandler);
+navToggleButton.addEventListener('click', navHandler);
